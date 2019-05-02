55a6e74cd63ee431047f87441012085082ca7db5
Akira Shinohara <k017c1067@it-neec.jp>
2019-05-03 00:30:00 +0900

    [change] 斧の耐久値が木を切り倒すのに不十分だった場合、木こりを発火させないように

--- src/work/mojamoja/logging/Cutter.kt
+++ src/work/mojamoja/logging/Cutter.kt
@@ -1,13 +1,10 @@
 package work.mojamoja.logging
 
-import org.bukkit.Bukkit
-import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
-import org.bukkit.inventory.ItemStack
-import org.bukkit.util.Vector
+import org.bukkit.inventory.meta.Damageable
 
 class Cutter: Listener {
     @EventHandler fun listen(e: BlockBreakEvent) {
@@ -16,42 +13,25 @@ class Cutter: Listener {
         val tool = player.inventory.itemInMainHand
 
         // 対象のブロック、ツールでなかった場合 もしくはスニーク中の場合はアーリーリターン
-        if (!(isLog(block) && isAxe(tool)) || player.isSneaking) {
-            return
-        }
+        if (!(isLog(block) && isAxe(tool)) || player.isSneaking) return
+
+
+        // この時点で斧であることが保証されているのでアンセーフキャスト
+        val damageable = tool.itemMeta as Damageable
+        val durability = tool.type.maxDurability - damageable.damage
+        val tree: ArrayList<Block> = ArrayList()
+
+        treeParser(block, tree)
+
+        // 斧の耐久値が木を切り倒すのに足りなかった場合はアーリーリターン
+        if (durability < tree.size) return
+
 
         e.isCancelled = true
-        breakWithMainHand(player, block)
 
-        // 起点のブロックから周囲17ブロック(起点を中心とした3x3の立方体から起点以下のブロックを除外)を探索し、対象のブロックであった場合にBlockBreakEventを呼び出す
-        // 隣接していないブロック（斜め位置）も破壊されるが。アカシアの木などの木こりに必要なので仕様とする
-        for (x in -1..1) {
-            for (y in 0..1) {
-                for (z in -1..1) {
-                    val nextBlock = block.location.add(Vector(x, y, z)).block
-
-                    if (isLog(nextBlock)) {
-                        val blockBreakEvent = BlockBreakEvent(nextBlock, player)
-                        Bukkit.getServer().pluginManager.callEvent(blockBreakEvent)
-                    }
-                }
-            }
+        // 木こり
+        for (log in tree) {
+            breakWithMainHand(player, log)
         }
-    }
-
-    private fun isLog(block: Block): Boolean {
-        return block.type == Material.OAK_LOG ||
-               block.type == Material.ACACIA_LOG ||
-               block.type == Material.BIRCH_LOG ||
-               block.type == Material.DARK_OAK_LOG ||
-               block.type == Material.JUNGLE_LOG ||
-               block.type == Material.SPRUCE_LOG
-    }
-
-    private fun isAxe(tool: ItemStack): Boolean {
-        return tool.type == Material.WOODEN_AXE ||
-               tool.type == Material.STONE_AXE ||
-               tool.type == Material.IRON_AXE ||
-               tool.type == Material.DIAMOND_AXE
     }
 }
--- src/work/mojamoja/logging/Utils.kt
+++ src/work/mojamoja/logging/Utils.kt
@@ -1,10 +1,13 @@
 package work.mojamoja.logging
 
+import org.bukkit.Material
 import org.bukkit.Sound
 import org.bukkit.block.Block
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
 import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.util.Vector
 
 fun breakWithMainHand(player: Player, block: Block) {
     val tool = player.inventory.itemInMainHand
@@ -26,4 +29,39 @@ fun breakWithMainHand(player: Player, block: Block) {
             tool.itemMeta = itemMeta
         }
     }
+}
+
+fun treeParser(block: Block, tree: ArrayList<Block>) {
+    // すでに探索済みのブロックであった場合何もせずにリターン
+    if (tree.indexOf(block) != -1) return
+
+    tree.add(block)
+
+    for (x in -1..1) {
+        for (y in 0..1) {
+            for (z in -1..1) {
+                val nextBlock = block.location.add(Vector(x, y, z)).block
+
+                if (isLog(nextBlock)) {
+                    treeParser(nextBlock, tree)
+                }
+            }
+        }
+    }
+}
+
+fun isLog(block: Block): Boolean {
+    return  block.type == Material.OAK_LOG ||
+            block.type == Material.ACACIA_LOG ||
+            block.type == Material.BIRCH_LOG ||
+            block.type == Material.DARK_OAK_LOG ||
+            block.type == Material.JUNGLE_LOG ||
+            block.type == Material.SPRUCE_LOG
+}
+
+fun isAxe(tool: ItemStack): Boolean {
+    return  tool.type == Material.WOODEN_AXE ||
+            tool.type == Material.STONE_AXE ||
+            tool.type == Material.IRON_AXE ||
+            tool.type == Material.DIAMOND_AXE
 }
